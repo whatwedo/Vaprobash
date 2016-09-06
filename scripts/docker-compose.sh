21a25d590e7b2d22a5665692304ad84585da54a2
Ueli Banholzer <ueli@whatwedo.ch>
2016-09-06 11:01:44 +0200

    fix(docker-compose): fix download url

--- scripts/docker-compose.sh
+++ scripts/docker-compose.sh
@@ -5,7 +5,7 @@ echo ">>> Installing docker-compose"
 #Install docker-compose
 lastReleasesUrl="https://github.com/docker/compose/releases.atom"
 latestDockerComposeVersion=$(wget -q -O- $lastReleasesUrl | \
-        egrep -m1 -o '/docker/compose/releases/tag/([0-9]\.[0-9]\.[0-9])"' | \
+        egrep -m1 -o '/docker/compose/releases/tag/([0-9]\.[0-9]\.[0-9])' | \
         egrep -o '([0-9]\.[0-9]\.[0-9])')
 curl -L -s https://github.com/docker/compose/releases/download/${latestDockerComposeVersion}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && \
         chmod +x /usr/local/bin/docker-compose
