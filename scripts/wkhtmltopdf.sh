cba93d8c74f298780a458dd010ed098d0bf08af7
Jonas Mosimann <jonasmosi@gmail.com>
2017-06-06 13:33:29 +0200

    update wkhtmltopdf mirror

--- scripts/wkhtmltopdf.sh
+++ scripts/wkhtmltopdf.sh
@@ -4,6 +4,6 @@ echo ">>> Installing wkhtmltopdf"
 
 # Install wkhtmltopdf
 apt-get install -qq wkhtmltopdf xvfb xfonts-75dpi
-wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+wget https://downloads.wkhtmltopdf.org/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
 dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
 rm wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
