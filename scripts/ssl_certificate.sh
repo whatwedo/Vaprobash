76a5d18dcce9b3e217f7895520f072e230533303
Marc Wieland <marcwieland95@gmail.com>
2018-10-02 16:09:39 +0200

    fix(ssl): Update script and allow prepared cert in folder which are just copied over

--- scripts/ssl_certificate.sh
+++ scripts/ssl_certificate.sh
@@ -13,19 +13,20 @@ fi
 SYSTEM_SSL_PATH="/etc/ssl/vaprobash"
 
 # Clean previously generated ssl folder
-sudo rm -rf "$SYSTEM_SSL_PATH" && sudo mkdir "$SYSTEM_SSL_PATH"
+sudo rm -rf $SYSTEM_SSL_PATH && sudo mkdir $SYSTEM_SSL_PATH
 if [ -d "$CONFIG_SSL_PATH" ]; then
-	sudo cp -r "$CONFIG_SSL_PATH/*" "$SYSTEM_SSL_PATH"
+	sudo cp -r $CONFIG_SSL_PATH/* $SYSTEM_SSL_PATH
 fi
 
 # Generate certificate from CA
 if [[ $CA_FILENAME != "false" ]]; then
-	cd "$SYSTEM_SSL_PATH"
+	cd $SYSTEM_SSL_PATH
 	sudo openssl genrsa -out $DOMAIN.key 2048
-	sudo openssl req -new -config $DOMAIN.conf -keyout $CA_FILENAME.key -out $DOMAIN.csr
-	sudo openssl x509 -req -in $DOMAIN.csr -CA $CA_FILENAME.pem -CAkey $CA_FILENAME.key -CAcreateserial -out $DOMAIN.crt -days 1825 -sha256 -extfile $DOMAIN.ext -passin file:passphrase.txt
-
-	sudo cp $DOMAIN.crt ../xip.io/xip.io.crt
-	sudo cp $DOMAIN.csr ../xip.io/xip.io.csr
-	sudo cp $DOMAIN.key ../xip.io/xip.io.key
+	sudo openssl req -new -config $DOMAIN.conf -keyout $DOMAIN.key -out $DOMAIN.csr
+	sudo openssl x509 -req -in $DOMAIN.csr -CA $CA_FILENAME.pem -CAkey $CA_FILENAME.key -CAcreateserial -out $DOMAIN.crt -days 365 -sha256 -extfile $DOMAIN.ext -passin file:passphrase.txt
 fi
+
+# Copy cert (have to be DOMAIN.EXT) files and replace xip once
+sudo cp $DOMAIN.crt ../xip.io/xip.io.crt
+sudo cp $DOMAIN.csr ../xip.io/xip.io.csr
+sudo cp $DOMAIN.key ../xip.io/xip.io.key
