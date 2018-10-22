a155f4ed9b07de16da6e9c14ac377dd8957e668d
Lukas Lücke <lukasluecke@users.noreply.github.com>
2018-10-22 14:08:23 +0200

    fix(mkdocs): update apt repositories after adding

--- scripts/mkdocs.sh
+++ scripts/mkdocs.sh
@@ -3,7 +3,7 @@
 echo ">>> Installing mkdocs"
 
 # Update python to 2.7.9+ (SSL updates required for mkdocs dependency tornado)
-add-apt-repository ppa:fkrull/deadsnakes-python2.7
+add-apt-repository ppa:fkrull/deadsnakes-python2.7 && apt-get update
 
 # Install pip
 apt-get install -qq python-pip python-dev
