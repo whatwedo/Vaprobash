6ef0efd1367c7b9862b2b6eb23518fd3ea6e0d1a
Ueli Banholzer <ueli@whatwedo.ch>
2019-08-27 00:40:56 +0200

    Update rvm.sh

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -29,7 +29,9 @@ if [[ $RVM_IS_INSTALLED -eq 0 ]]; then
 else
     # Import Michal Papis' key to be able to verify the installation
     echo ">>> Importing rvm public key"
-    gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    apt-get update
+    apt-get install gnupg2 -y
+    gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
     # Install RVM and install Ruby
     if [[ $RUBY_VERSION =~ "latest" ]]; then
@@ -45,7 +47,7 @@ else
     fi
 
     # Re-source RVM
-    . /home/vagrant/.rvm/scripts/rvm
+    source /usr/local/rvm/scripts/rvm
 
     # Re-source .profile if exists
     if [[ -f "/home/vagrant/.profile" ]]; then
