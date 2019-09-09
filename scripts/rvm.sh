11857ec5e19d1bef9358ded48a16880c8c5aa394
Bert-Jan Fikse <s4mpl3d@gmail.com>
2019-09-09 15:50:26 +0200

    fix(rvm.sh): sudo for apt

--- scripts/rvm.sh
+++ scripts/rvm.sh
@@ -29,8 +29,8 @@ if [[ $RVM_IS_INSTALLED -eq 0 ]]; then
 else
     # Import Michal Papis' key to be able to verify the installation
     echo ">>> Importing rvm public key"
-    apt-get update
-    apt-get install gnupg2 -y
+    sudo apt-get update
+    sudo apt-get install gnupg2 -y
     gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
     # Install RVM and install Ruby
